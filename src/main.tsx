109047ae9801f514b4d34d6de03926a9c8a38648
chandrashekhar <chandrashekharkumbarias8055@gmail.com>
2026-03-17 20:02:07 +0530

    404 error solved

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const params = new URLSearchParams(window.location.search);
+const redirectPath = params.get("redirect");
+if (redirectPath && redirectPath.startsWith("/")) {
+  window.history.replaceState({}, "", redirectPath);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
